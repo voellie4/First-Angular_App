63a16bb9f474d31c4a90b792df9821b308c094b1
Ellie Vo <vo.ellie@gmail.com>
2022-08-21 19:25:20 -0500

    add server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //always register new component here
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //if a component is used in index.html, add here
 })
 export class AppModule { }
